386a260cf75902b94fdfa49c03d31cb44db0f5c1
deez <nhatvan2231@gmail.com>
2025-04-26 18:44:19 -0500

    minor update

--- src/tools/run_all.sh
+++ src/tools/run_all.sh
@@ -6,5 +6,5 @@ for dir in ~/AA_DOA/src/data/rss360_noise/*.dat; do
 	#echo $OUTPUT
 	cat ~/ncpa/libNCPA/data/xstream_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n 96000 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 -gD > "$OUTPUT"
 	#OUTPUT=$(echo ${dir} | sed 's/.dat/.bin/')
-	#cat ~/ncpa/libNCPA/data/xstream_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n 96000 | ./a.out --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 > "$OUTPUT"
+	#cat ~/AA_DOA/src/tools/signal_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n -1 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 > "$OUTPUT"
 done
