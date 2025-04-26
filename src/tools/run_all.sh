b961b1d5f65a668e0c80b2d21f1d2ad90a590c16
deez <nhatvan2231@gmail.com>
2025-04-26 18:56:25 -0500

    commit

--- src/tools/run_all.sh
+++ src/tools/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #OUTPUT="${OUTPUT:=test.txt}"
-for dir in ~/AA_DOA/src/data/rss360_noise/*.dat; do
+for dir in ~/AA_DOA/src/data/raw_rss360/*.dat; do
 	echo "\n${dir}"
-	OUTPUT=$(echo ${dir} | sed 's/\.dat/_tau.bin/')
+	#OUTPUT=$(echo ${dir} | sed 's/\.dat/_tau.bin/')
 	#echo $OUTPUT
-	cat ~/ncpa/libNCPA/data/xstream_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n 96000 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 -gD > "$OUTPUT"
-	#OUTPUT=$(echo ${dir} | sed 's/.dat/.bin/')
-	#cat ~/AA_DOA/src/tools/signal_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n -1 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 > "$OUTPUT"
+	#cat ~/ncpa/libNCPA/data/xstream_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n 96000 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 -gD > "$OUTPUT"
+	OUTPUT=$(echo ${dir} | sed 's/\.dat/_raw.bin/')
+	cat ~/AA_DOA/src/tools/signal_1khz.bin | ~/ncpa/libNCPA/bin/binary2human --type double --N_channels 1 | head -n -1 | ./noisePropagator --arrayGeometry AA_geometry.tsv --signalInfo "${dir}" --dataSample 100 > "$OUTPUT"
 done
